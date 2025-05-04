c8c959a7a45e0b1f16f678b8a259f79be0919a27
Alvaro Morales <moralestombe19@gmail.com>
2025-05-03 23:31:02 -0500

    Se agregan los cambios que se realizaron la capa Model de user usarios activo e inactivo

--- Bibliotecavm-app/Model/User.cs
+++ Bibliotecavm-app/Model/User.cs
@@ -9,9 +9,10 @@
         public string Contrasena { get; set; }
         public string Salt { get; set; }
         public string Rol { get; set; }
+        public string Estado { get; set; } 
 
-        // Constructor con parámetros
-        public User(int usuId, string nombreCompleto, string correo, string contrasena, string salt, string rol)
+        // Constructor con parámetros (actualizado)
+        public User(int usuId, string nombreCompleto, string correo, string contrasena, string salt, string rol, string estado)
         {
             UsuId = usuId;
             NombreCompleto = nombreCompleto;
@@ -19,11 +20,14 @@
             Contrasena = contrasena;
             Salt = salt;
             Rol = rol;
+            Estado = estado; // Añadido el parámetro estado
         }
 
         // Constructor vacío
         public User()
         {
+            // Inicialización opcional de valores por defecto
+            Estado = "Activo"; // Puedes establecer un valor por defecto si lo deseas
         }
     }
 }
